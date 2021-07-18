ccb909b1724c415d9df0ff848a5d76d8be5da2ce
Bohan Hu <hubohancser@gmail.com>
2021-07-19 01:00:01 +0800

    Add usertests and other things for test

--- proc0/src/main.rs
+++ proc0/src/main.rs
@@ -6,68 +6,103 @@
 #[macro_use]
 extern crate oshit_usrlib;
 
-use oshit_usrlib::{
-    sys_fork,
-    sys_waitpid,
-    sys_exec,
-    sys_yield,
-    sys_getpid,
-    sys_exit
-};
 
-const programs : &'static [&'static [u8]] = &[
-    b"/shell\0", 
-    // b"/brk\0", 
-    // b"/chdir\0",
-    // b"/clone\0",
-    // b"/close\0",
-    // b"/dup\0",
-    // b"/dup2\0",
-    // b"/execve\0",
-    // b"/exit\0",
-    // b"/fork\0",
-    // b"/fstat\0",
-    // b"/getcwd\0",
-    // b"/getdents\0",
-    // b"/getpid\0",
-    // b"/getppid\0",
-    // b"/gettimeofday\0",
-    // b"/mkdir_\0",
-    // b"/mmap\0",
-    // b"/mount\0",
-    // b"/munmap\0",
-    // b"/open\0",
-    // b"/openat\0",
-    // b"/pipe\0",
-    // b"/read\0",
-    // b"/sleep\0",
-    // b"/times\0",
-    // b"/umount\0",
-    // b"/uname\0",
-    // b"/unlink\0",
-    // b"/wait\0",
-    // b"/waitpid\0",
-    // b"/write\0",
-    // b"/yield\0"
-];
+use oshit_usrlib::{print, sys_exec, sys_exit, sys_fork, sys_getpid, sys_waitpid, sys_yield};
+
+const busybox_commands : &'static [&'static [u8]] = &[
+    b"echo \"#### independent command test\"\0",
+    b"ash -c exit\0",
+    b"sh -c exit\0",
+    b"basename /aaa/bbb\0",
+    b"cal\0",
+    b"clear\0",
+    b"date \0",
+    b"df \0",
+    b"dirname /aaa/bbb\0",
+    b"dmesg \0",
+    b"du\0",
+    b"expr 1 + 1\0",
+    b"false\0",
+    b"true\0",
+    b"which ls\0",
+    b"uname\0",
+    b"uptime\0",
+    b"printf \"abc\n\"\0",
+    b"ps\0",
+    b"pwd\0",
+    b"free\0",
+    b"hwclock\0",
+    b"kill 10\0",
+    b"ls\0",
+    b"sleep 1\0",
+    b"echo \"#### file opration test\"\0",
+    b"touch test.txt\0",
+    b"echo \"hello world\" > test.txt\0",
+    b"cat test.txt\0",
+    b"cut -c 3 test.txt\0",
+    b"od test.txt\0",
+    b"head test.txt\0",
+    b"tail test.txt \0",
+    b"hexdump -C test.txt \0",
+    b"md5sum test.txt\0",
+    b"echo \"ccccccc\" >> test.txt\0",
+    b"echo \"bbbbbbb\" >> test.txt\0",
+    b"echo \"aaaaaaa\" >> test.txt\0",
+    b"echo \"2222222\" >> test.txt\0",
+    b"echo \"1111111\" >> test.txt\0",
+    b"echo \"bbbbbbb\" >> test.txt\0",
+    b"sort test.txt | ./busybox uniq\0",
+    b"stat test.txt\0",
+    b"strings test.txt \0",
+    b"wc test.txt\0",
+    b"[ -f test.txt ]\0",
+    b"more test.txt\0",
+    b"rm test.txt\0",
+    b"mkdir test_dir\0",
+    b"mv test_dir test\0",
+    b"rmdir test\0",
+    b"grep hello busybox_cmd.txt\0",
+    b"cp busybox_cmd.txt busybox_cmd.bak\0",
+    b"rm busybox_cmd.bak\0",
+    b"find -name \"busybox_cmd.txt\"\0",
+    ];
 
 #[no_mangle]
 fn main() -> i32 {
     println!("[proc0] Started.");
     if sys_fork() == 0 {
-        for prog in programs {
-            println!("This is {}, Now execute: {}", sys_getpid(), core::str::from_utf8(prog).unwrap());
-            let child = sys_fork();
-            if child == 0 {
-                if sys_exec(prog.as_ptr(), &[0 as *const u8], &[0 as *const u8]) == -1 {
-                    println!("Exec failed. dying.");
-                    sys_exit(-1);
-                }
-            } else {
-                let mut exit_code: i32 = 0;
-                sys_waitpid(child, &mut exit_code);
-                println!("Finished {}", core::str::from_utf8(prog).unwrap());
+        // let rst = "result.txt";
+        // println!("If the CMD runs incorrectly, return value will put in {}", rst);
+        // println!("Else nothing will put in {}", rst);
+        // println!("TEST START");
+        // for command in busybox_commands {
+        //     let child = sys_fork();
+        //     let args = [
+        //         command.as_ptr(),
+        //         0 as *const u8
+        //     ];
+        //     if child == 0 {
+        //         let ret = sys_exec(b"/busybox".as_ptr(), &args, &[0 as *const u8]);
+        //         if ret != 0 && (command.ne(b"false\0")) {
+        //             println!("testcase busybox {} fail", core::str::from_utf8(command).unwrap());
+        //             println!("return: {}, cmd: {}", ret, core::str::from_utf8(command).unwrap());   // TODO: Write to file
+        //             sys_exit(-1);
+        //         }
+        //     } else {
+        //         let mut exit_code: i32 = 0;
+        //         sys_waitpid(child, &mut exit_code);
+        //         println!("testcase busybox {} success", core::str::from_utf8(command).unwrap());
+        //     }
+        // }
+        let child = sys_fork();
+        if child == 0 {
+            let ret = sys_exec(b"/usertests\0".as_ptr(), &[0 as *const u8], &[0 as *const u8]);
+            if ret != 0  {
+                sys_exit(-1);
             }
+        } else {
+            let mut exit_code: i32 = 0;
+            sys_waitpid(child, &mut exit_code);
         }
     } else {
         loop {
@@ -77,11 +112,7 @@ fn main() -> i32 {
                 sys_yield();
                 continue;
             } 
-            println!(
-                "[proc0] Released a zombie process, pid={}, exit_code={}",
-                pid,
-                exit_code,
-            );
+            println!("TEST END");   // TODO: Write to file
         }
     }
     0
---
+++ usertests/kernel/syscall.h
@@ -0,0 +1,22 @@
+// System call numbers
+#define SYS_fork    220
+#define SYS_exit    93
+#define SYS_wait    260
+#define SYS_pipe    59
+#define SYS_read    63
+#define SYS_kill    129    // TODO
+#define SYS_exec    221
+#define SYS_fstat   80
+#define SYS_chdir   49
+#define SYS_dup     23
+#define SYS_getpid  172
+#define SYS_sbrk    214
+#define SYS_sleep   101
+#define SYS_uptime  14  // TODO
+#define SYS_open    56
+#define SYS_write   64
+#define SYS_mknod   17  // TODO
+#define SYS_unlink  35
+#define SYS_link    37
+#define SYS_mkdir   34
+#define SYS_close   57
